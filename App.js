86e5207f65d6a2bf4ee6942d548533f4683e3b3d
Michael Marcinek <michaelmarcinek@gmail.com>
2017-10-24 15:55:28 -0400

    updates dependencies

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import AppContainer from './app/containers/AppContainer'; 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReduxers, compose } from 'redux';
-import { thunkMiddleware } from 'redux-thunk';
-import { createLoggger } from 'redux-logger';
-import reducer from './app/reducers'; 
-import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import reducer from './app/reducers'
+// import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
-const loggerMiddleware = createLoggger({ predicate: (getState, action ) => __DEV__ });
+const loggerMiddleware = createLogger({ predicate: (getState, action ) => __DEV__ });
 
 function configureStore(initialState) {
   const enhancer = compose(
@@ -20,25 +22,24 @@ function configureStore(initialState) {
 
 const store = configureStore({});
 
-import { Platform, StyleSheet, Text, View } from 'react-native';
 
-let mock = require('./mock.json');
+// let mock = require('./mock.json');
 
-const tableHead = Object.keys(mock.table[0]);
-const tableData = [];
+// const tableHead = Object.keys(mock.table[0]);
+// const tableData = [];
 
-class FormUpload extends Component{
-  render() {
-    return (
-      <View style={styles.container}>
-        <Table style={styles.table} borderStyle={{borderWidth: 0.5, borderColor: '#c8e1ff'}}>
-          <Row data={tableHead} style={styles.head} textStyle={styles.text} flexArr={[4, 4, 4, 4, 2, 2, 2, 2, 6]}/>
-          <Cols data={tableData} textStyle={styles.text} heightArr={[20, 50]} flexArr={[4, 4, 4, 4, 2, 2, 2, 2, 6]}/>
-        </Table>
-      </View>
-    );
-  }
-}
+// class FormUpload extends Component{
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Table style={styles.table} borderStyle={{borderWidth: 0.5, borderColor: '#c8e1ff'}}>
+//           <Row data={tableHead} style={styles.head} textStyle={styles.text} flexArr={[4, 4, 4, 4, 2, 2, 2, 2, 6]}/>
+//           <Cols data={tableData} textStyle={styles.text} heightArr={[20, 50]} flexArr={[4, 4, 4, 4, 2, 2, 2, 2, 6]}/>
+//         </Table>
+//       </View>
+//     );
+//   }
+// }
 
 const App = () => (
   <Provider store={store}>
@@ -48,35 +49,35 @@ const App = () => (
 
 AppRegistry.registerComponent('FormUpload', () => App); 
 
-const styles = StyleSheet.create({
-  table: { 
-    width: 660,
-   },
-  head: { height: 40, backgroundColor: '#f1f8ff' },
-  text: { textAlign: 'center' },
-  title: {
-    fontSize: 30,
-    alignSelf: 'center',
-    marginBottom: 30
-  },
-  buttonText: {
-    fontSize: 18,
-    color: 'white',
-    alignSelf: 'center'
-  },
-  button: {
-    height: 36,
-    backgroundColor: '#48BBEC',
-    borderColor: '#48BBEC',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 10,
-    alignSelf: 'stretch',
-  },
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   table: { 
+//     width: 660,
+//    },
+//   head: { height: 40, backgroundColor: '#f1f8ff' },
+//   text: { textAlign: 'center' },
+//   title: {
+//     fontSize: 30,
+//     alignSelf: 'center',
+//     marginBottom: 30
+//   },
+//   buttonText: {
+//     fontSize: 18,
+//     color: 'white',
+//     alignSelf: 'center'
+//   },
+//   button: {
+//     height: 36,
+//     backgroundColor: '#48BBEC',
+//     borderColor: '#48BBEC',
+//     borderWidth: 1,
+//     borderRadius: 8,
+//     marginBottom: 10,
+//     alignSelf: 'stretch',
+//   },
+//   container: {
+//     flex: 1,
+//     padding: 20,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//   },
+// });
--- app/lib/api.js
+++ app/lib/api.js
@@ -24,8 +24,8 @@ class Api {
   }
   
   static xhr(route, params, verb) {
-  //   const host = 'http://www.recipepuppy.com'
-  //   const url = `${host}${route}`
+    const host = 'http://www.recipepuppy.com'
+    const url = `${host}${route}`
     let options = Object.assign({ method: verb }, params ? { body: JSON.stringify(params) } : null );
     options.headers = Api.headers()
     return fetch(url, options).then( resp => {
