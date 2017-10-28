5425c66a046678f8c78f37bc9a6e2e6d6e7563dc
Michael Marcinek <michaelmarcinek@gmail.com>
2017-10-28 14:54:43 -0400

    jobs list function, begin job form

--- app/containers/JobList.js
+++ app/containers/JobList.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import ReactNative from 'react-native';
 import { ActionCreators } from '../actions';
 import realm from '../store/realm';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+
 
 const {
   ScrollView,
@@ -11,27 +13,24 @@ const {
   TextInput,
   TouchableHighlight,
   StyleSheet,
-  ListView
+  Modal
 } = ReactNative
 
+
+
 class JobList extends Component {
-  constructor() {
-    super();
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
-    };  
+  constructor(props) {
+    super(props);
 
-    this.jobResults = realm.objects('Job').sorted('createdAt');
-
+    this.jobResults = Array.from(realm.objects('Job').sorted('createdAt'));
     if (this.jobResults.length < 1) {
       realm.write(() => {
         realm.create('Job', {
-          job_id: 'ed43214', 
+          job_id: 'ed441234', 
           uploaded: false, 
-          company: 'Test Co.', 
-          address_1: '123 test highway',
-          address_2: 'Ste 123',
+          company: 'Foo Company', 
+          address_1: '456 Buford highway',
+          address_2: '',
           city: 'Atlanta',
           state: 'GA',
           zipcode: 30307,
@@ -41,22 +40,69 @@ class JobList extends Component {
       })
     }
     
-    this.jobResults.addListener((name, changes) => {
-      console.log("changed: " + JSON.stringify(changes));
-    });
+    this.jobList = this.jobResults.map((job) => {
+      return(
+        <View onPress={this.jobSelect} key={job.job_id} style={{flexDirection: 'row', paddingTop: 20, paddingLeft:20, borderBottomColor: '#d3d3d3', borderBottomWidth: 1}}>
+          <TouchableHighlight style={{width: 75, height: 40}} onPress={this.jobSelect}>
+            <Text>Select</Text>
+          </TouchableHighlight>
+          <Text style={{width: 150, height: 40}}>{job.job_id}</Text>
+          <Text style={{width: 150, height: 40}}>{job.company}</Text>
+        </View>
+      )
+    })
 
-    this.state = {};
+    this.state = {
+      modalVisible: false,
+    }
+
   }
 
-  addJob() {
-    console.log(this.jobResults)
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
   }
 
-  render(){
+  jobSelect(){
+  
+  }
+
+  render(){ 
     return (
-      <TouchableHighlight onPress={this.addJob} style={{marginTop:20, marginLeft: 20, height: 30}}>
-        <Text>Add a Job</Text>
-      </TouchableHighlight>
+      <View>
+        <Text style={{textAlign: 'center', fontSize: 15, padding: 15}}>Jobs</Text>
+        <View style={{flexDirection: 'row', paddingLeft: 20, borderBottomColor: '#d3d3d3', borderBottomWidth: 1}}>
+          <Text style={{width: 75, height: 40}}></Text>
+          <Text style={{width: 150, height: 40}}>JOB ID</Text>
+          <Text style={{width: 150, height: 40}}>Company</Text>
+        </View> 
+        <View>
+          {this.jobList}
+        </View>
+        <TouchableHighlight onPress={() => {
+          this.setModalVisible(!this.state.modalVisible)
+        }} style={{marginTop:20, marginLeft: 20, height: 30}}>
+          <Text>Add a Job</Text>
+        </TouchableHighlight>
+        <Modal
+        animationType="slide"
+        transparent={false}
+        visible={this.state.modalVisible}
+        onRequestClose={() => {alert("Modal has been closed.")}}
+        >
+        <View style={{marginTop: 22}}>
+          <View>
+            <Text>Hello World!</Text>
+
+            <TouchableHighlight onPress={() => {
+              this.setModalVisible(!this.state.modalVisible)
+            }}>
+              <Text>Hide Modal</Text>
+            </TouchableHighlight>
+
+          </View>
+        </View>
+      </Modal>  
+      </View>
     )
   }  
 }
@@ -71,6 +117,5 @@ const mapStateToProps = (state, props) => ({
 const mapDispatchToProps = {
   // ...ActionCreators
 }
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(JobList);
